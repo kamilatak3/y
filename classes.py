4c9a253c68cd9fe6f234315edd2c4cd19265265b
Kamil Atak <kamilatakw@gmail.com>
2024-01-28 04:04:48 -0600

    first commit

---
+++ classes.py
@@ -0,0 +1,25 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        return f"My name is {self.name} and I am {self.age} years old."
+
+class Student(Person):
+    def __init__(self, name, age, student_id, grade):
+        super().__init__(name, age)
+        self.student_id = student_id
+        self.grade = grade
+
+    def study(self):
+        return f"I'm studying in grade {self.grade}."
+
+class Teacher(Person):
+    def __init__(self, name, age, employee_id, subject):
+        super().__init__(name, age)
+        self.employee_id = employee_id
+        self.subject = subject
+
+    def teach(self):
+        return f"I teach {self.subject}."
---
+++ main.py
@@ -0,0 +1,9 @@
+from classes import Student, Teacher
+
+# student1 = Student("Alice", 15, "S123", 10)
+# print(student1.introduce())
+# print(student1.study())
+
+# teacher1 = Teacher("Mr. Brown", 40, "T456", "Mathematics")
+# print(teacher1.introduce())
+# print(teacher1.teach())
